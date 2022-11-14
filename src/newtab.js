1878d6c286d465df7e4136046fa2ceb8c4f22301
kmgb <25809479+kmgb@users.noreply.github.com>
2022-11-14 16:51:27 -0500

    Sanitize HTML with DOMPurify

--- src/newtab.js
+++ src/newtab.js
@@ -5,12 +5,16 @@ function loadContent() {
 	browser.storage.local.get({'url': '', 'sync': true}).then(function(localstorage) {
 		if(localstorage.sync === true) {
 			browser.storage.sync.get({'url':''}).then(function(syncedstorage) {
-				document.body.innerHTML = syncedstorage.url;
+				document.body.innerHTML = sanitizeHtml(syncedstorage.url);
 			});
 		} else {
-			document.body.innerHTML = localstorage.url;
+			document.body.innerHTML = sanitizeHtml(localstorage.url);
 		}
 	});
+}
+
+function sanitizeHtml(x) {
+	return DOMPurify.sanitize(x, {FORCE_BODY: true});
 }
 
 loadContent();
